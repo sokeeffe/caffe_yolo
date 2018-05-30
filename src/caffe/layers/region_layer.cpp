6f7286511ccf27a38d019e5c58476986cdce94b2
Simon O'Keeffe <simon.okeeffe.2010@nuim.ie>
2018-05-30 13:46:05 +0100

    Forward Pass of Region Layer

--- src/caffe/layers/region_layer.cpp
+++ src/caffe/layers/region_layer.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
 #include <vector>
+#include <cmath>
+#include <cfloat>
 
 #include "caffe/layers/region_layer.hpp"
 #include "caffe/util/math_functions.hpp"
@@ -9,6 +11,13 @@ namespace caffe {
 template <typename Dtype>
 inline Dtype sigmoid(Dtype x) {
   return 1. / (1. + exp(-x));
+}
+
+inline int entry_index(int side, int num_classes, int num, int coords, int batch, int location, int entry)
+{
+    int n =   location / (side*side);
+    int loc = location % (side*side);
+    return batch*side*side*num*(coords+num_classes+1) + n*side*side*(coords+num_classes+1) + entry*side*side + loc;
 }
 
 template <typename Dtype>
@@ -46,28 +55,39 @@ void RegionLayer<Dtype>::Forward_cpu(
   const Dtype* bottom_data = bottom[0]->cpu_data();
   Dtype* top_data = top[0]->mutable_cpu_data();
   caffe_copy(bottom[0]->count(), bottom_data, top_data);
+  for (int b = 0; b < bottom[0]->num(); b++) {
+    for (int n = 0; n < num_; n++) {
+      int index = entry_index(side_, num_classes_, num_, coords_, b, n*side_*side_, 0);
+      for (int i = index; i < index+(side_*side_*2); ++i) {
+        top_data[i] = sigmoid(top_data[i]);
+      }
+      index = entry_index(side_, num_classes_, num_, coords_, b, n*side_*side_, coords_);
+      for (int i = index; i < index+(side_*side_); ++i) {
+        top_data[i] = sigmoid(top_data[i]);
+      }
+      index = entry_index(side_, num_classes_, num_, coords_, b, n*side_*side_, coords_ + 1);
+      for (int g = 0; g < side_*side_; ++g) {
+        float sum = 0;
+        float largest = -FLT_MAX;
+        for (int i = 0; i < num_classes_; ++i) {
+          if (top_data[index+g+(i*side_*side_)] > largest) largest = top_data[index+g+(i*side_*side_)];
+        }
+        for (int i = 0; i < num_classes_; ++i) {
+          float e = exp(top_data[index+g+(i*side_*side_)] - largest);
+          sum += e;
+          top_data[index+g+(i*side_*side_)] = e;
+        }
+        for (int i = 0; i < num_classes_; ++i) {
+          top_data[index+g+(i*side_*side_)] /= sum;
+        }
+      }
+    }
+  }
 }
 
 template <typename Dtype>
 void RegionLayer<Dtype>::Backward_cpu(const vector<Blob<Dtype>*>& top,
       const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom) {
-  if (propagate_down[1]) {
-    LOG(FATAL) << this->type()
-               << " Layer cannot backpropagate to label inputs.";
-  }
-  if (propagate_down[0]) {
-    const Dtype sign(1.);
-    const Dtype alpha = sign * top[0]->cpu_diff()[0] / bottom[0]->num();
-    //const Dtype alpha(1.0);
-    //LOG(INFO) << "alpha:" << alpha;
-    
-  //   caffe_cpu_axpby(
-  //       bottom[0]->count(),
-  //       alpha,
-  //       real_diff_.cpu_data(),
-  //       Dtype(0),
-  //       bottom[0]->mutable_cpu_diff());
-  }
 }
 
 #ifdef CPU_ONLY
